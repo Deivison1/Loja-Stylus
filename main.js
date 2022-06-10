a3a2d78342c148d40be0e94315369994300ead3f
Deivison1 <santosdeivison480@gmail.com>
2022-06-10 16:42:56 -0300

    Atualizando arquios json e criando pasta img

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
 fetch("./itens.json") 
     .then(res => { return res.json() }) 
-    .then(items => {
+    .then(items => { 
 
-
-incinializarLoja = () => {
-    var containerProdutos = document.getElementById('produtos');
-    items.map((val)=>{
-        containerProdutos.innerHTML += `
-        <div class="produto-single">
-            <img src="`+val.img+`"<img/>
-            <p>`+val.nome+`</p>
-            <p>Valor: R$ `+val.preco+`</p>
-            <a class= "link-carrinho"key="`+val.id+`" href="#carrinho">Adicionar ao carrinho!</a>
-        </div>
-
-        `;
-    })
-}
- atualizarCarrinho = () => { 
-    var total = 0 
-    var containerCarrinho = document.getElementById('carrinho'); 
- 
-    containerCarrinho.innerHTML = ""; 
- 
-    items.map((val) => { 
-        if (val.quantidade > 0) { 
-            containerCarrinho.innerHTML += ` 
-        <div class="info-carrinho"> 
-            <p>Produto: `+ val.nome + `</p> 
-            <p>Qtde: `+ val.quantidade + `</p> 
-            <p>Preço: `+ val.preco * val.quantidade + `</p> 
+        incinializarLoja = () => { 
+            var containerProdutos = document.getElementById('produtos'); 
+            items.map((val) => { 
+                containerProdutos.innerHTML += ` 
+        <div class="produto-single"> 
+            <img src="./img/`+ val.img + `"<img/> 
+            <p>`+ val.nome + `</p> 
+            <p>Valor: R$ `+ val.preco +`</p> 
+            <a class= "link-carrinho"key="`+ val.id + `" href="#carrinho">Adicionar ao carrinho!</a> 
         </div> 
-        ` 
-            total = total + val.preco * val.quantidade 
-            console.log(total) 
-        }; 
-    }) 
-}
-var links = document.getElementsByTagName('a');
+ 
+        `; 
+            }) 
+        } 
+        incinializarLoja(); 
+ 
+        atualizarCarrinho = () => { 
 
-    for(var i = 0; i < links.length; i++){
-        links[i].addEventListener('click', function(){
-            let key = this.getAttribute('key');
-            items[key].quantidade++;
-            atualizarCarrinho();
-            return false;
+            var total = 0;
+            var quantidade = 0;
+            var containerCarrinho = document.getElementById('carrinho'); 
+ 
+            containerCarrinho.innerHTML = ""; 
+ 
+            items.map((val) => { 
+                if (val.quantidade > 0) { 
+                    containerCarrinho.innerHTML += ` 
+            <div class="info-carrinho"> 
+                <p>Produto: `+ val.nome + `</p> 
+                <p>Qtde: `+ val.quantidade + `</p> 
+                <p>Preço: `+ val.preco * val.quantidade + `</p> 
+            </div> 
+        ` 
+        total = total + val.preco * val.quantidade 
+        quantidade = quantidade + val.quantidade 
+            }; 
         }) 
-    }
-   } 
+        containerCarrinho.innerHTML += 
+        `<p>Quantidade total: ${quantidade} Valor total:${total}</p>`;
+            
+        } 
+        var links = document.getElementsByTagName('a'); 
+ 
+        for (var i = 0; i < links.length; i++) { 
+            links[i].addEventListener('click', function () { 
+                let key = this.getAttribute('key'); 
+                items[key].quantidade++; 
+                atualizarCarrinho(); 
+                return false; 
+            }) 
+        } 
+ 
+    } 
 );
