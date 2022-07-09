cd58395d5c467daa36a641811009ee3a3d4ae11b
Deivison1 <santosdeivison480@gmail.com>
2022-07-09 11:52:02 -0300

    Adicionando botao de rolagem

--- js/main.js
+++ js/main.js
@@ -74,3 +74,10 @@ var containerCarrinho = document.getElementById('carrinho');
     
 });
 
+function scrollUp(){
+    
+    const scrollUp = document.getElementById('scroll-up');
+        if(this.scrollY >= 250) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    }
+window.addEventListener('scroll', scrollUp);
+
